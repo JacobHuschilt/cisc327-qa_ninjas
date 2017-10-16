a3dfd19dcb7e03e42b4dbe730b94c70aa45f513a
JacobHuschilt <j.huschilt@queensu.ca>
2017-10-16 18:26:38 -0400

    initial commit

--- src/com/qa_ninjas/Main.java
+++ src/com/qa_ninjas/Main.java
@@ -1,8 +1,24 @@
+/**
+ * Jacob Huschilt 10197679
+ * Heni Virág
+ * Gaveshini Sriyananda
+ *
+ * Created: 09/16/2017
+ *
+ * ©2017 QA_Ninjas
+ */
+
 package com.qa_ninjas;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+    }
+
+    public static void login(String accountsFileName, String tsfSummaryFileName) {
+        System.out.println("Please enter a session type: ");
+
+        String sessionType;
     }
 }
