8757e0adf7f88727ce7360e654889a2892e480f9
henivirag <13hv10@queensu.ca>
2017-10-18 15:02:24 -0400

    New Deposit Class

--- src/com/qa_ninjas/AccountUtilities.java
+++ src/com/qa_ninjas/AccountUtilities.java
@@ -2,16 +2,83 @@ package com.qa_ninjas;
 
 import java.util.ArrayList;
 
-public abstract class AccountUtilities {
+/**
+ * This class is a parent class to CreateAcct, DeleteAcct.  It contains methods
+ * that validate account numbers and account names and update the list of valid
+ * accounts.  The ArrayList called "accountList" takes ValidAccount objects.
+ */
+public class AccountUtilities {
     public boolean isAuthorized;
     ArrayList<ValidAccount> accountList = new ArrayList();
 
-    protected boolean isValidAcct(int acctNum) {
-        return false;
+    /**
+     * This method validates a given account number.  It prints an error message
+     * warning the user if the account number did not meet the requirements.
+     * @param acctNum
+     * @return true if the account number is valid, false otherwise.
+     */
+    protected boolean isValidAcct(String acctNum) {
+        try {
+            int acctNumInt = Integer.parseInt(acctNum);
+            if (acctNum.length() != 7) {
+                return false;
+            }
+            if (acctNum.charAt(0) == '0') {
+                return false;
+            }
+            for (ValidAccount singleAccount : accountList) {
+                if (acctNumInt == singleAccount.getAcctNum()) {
+                    return false;
+                }
+            }
+            return true;
+        } catch (NumberFormatException exception) {
+            System.out.println("Error! Invalid Account Number: " + exception);
+            return false;
+        }
     }
 
-    protected void updateAccountList(boolean isNew, int acctNum, String name) {
-        System.out.println();
+    /**
+     * This method validates a given account name.  It warns the user if the account
+     * name is too short, too long or contains non-alphabetic characters.
+     * @param name
+     * @return true if valid and false otherwise.
+     */
+    protected boolean isValidName(String name) {
+        for (int i = 0; i < name.length(); i++) {
+            if (!Character.isLetter(name.charAt(i))) {
+                System.out.println("Error! Invalid Account Name: " + name);
+                return false;
+            }
+        }
+        if (name.length() < 3 || name.length() > 30) {
+            System.out.println("Error! Account Name Too Long: " + name);
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * This method handles the addition or deletion of an account using the valid
+     * account list declared above. It simply handles updating and does not return
+     * anything. The third parameter "isNew" flag is set to true if the current
+     * account that the method is working with is in fact new, false otherwise.
+     * @param acctNum
+     * @param name
+     * @param isNew
+     */
+    protected void updateAccountList(int acctNum, String name, boolean isNew) {
+        if (isNew) { // adding new account
+            ValidAccount newAcct = new ValidAccount(acctNum, name, true);
+            accountList.add(newAcct);
+        } else { // removing existing account
+            for (ValidAccount singleAccount : accountList) {
+                // if the account number is found, delete that account object
+                if (acctNum == singleAccount.getAcctNum()){
+                    accountList.remove(singleAccount);
+                }
+            }
+        }
     }
 
 }
--- src/com/qa_ninjas/ValidAccount.java
+++ src/com/qa_ninjas/ValidAccount.java
@@ -1,7 +1,21 @@
 package com.qa_ninjas;
 
 public class ValidAccount {
-    public int acctNum;
-    public String name;
-    public boolean isNew;
+    private int acctNum;
+    private String name;
+    private boolean isNew;
+
+    public ValidAccount(int acctNum, String name, boolean isNew) {
+        this.acctNum = acctNum;
+        this.name = name;
+        this.isNew = isNew;
+    }
+
+    public int getAcctNum() {
+        return acctNum;
+    }
+
+    public boolean isNew() {
+        return isNew;
+    }
 }
